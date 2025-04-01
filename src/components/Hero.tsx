d921301be8d289c86b48bffa72d1f1fe9b465cd7
afrit-bagani <o6228466@gmail.com>
2025-04-02 00:51:36 +0530

    add header

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,46 +1,48 @@
 const API_KEY = import.meta.env.VITE_API_KEY;
-import { useEffect } from "react";
-console.log("API Key from .env:", API_KEY);
+import { useEffect, useState } from "react";
+
+type Movie = {
+  backdrop_path: string;
+  title: string;
+};
 
 function Hero() {
   const MOVIE_BASE_URL = "https://api.themoviedb.org/3/movie/popular";
-  // const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/original";
+  const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/original";
 
-  console.log("API Key:", API_KEY); // Debugging step
+  const [movie, setMovie] = useState<Movie | null>(null);
 
   useEffect(() => {
-    if (!API_KEY) {
-      console.error(
-        "API Key is missing. Check your .env file and restart Vite."
-      );
-      return;
-    }
-
     (async function getPopularMovies() {
       try {
-        const url =
-          "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1";
-        const options = {
-          method: "GET",
-          headers: {
-            accept: "application/json",
-            Authorization:
-              "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzNGU3ZDc5MTZkZThmZGZjMTJmMTVjYTUzZTE0NTIxYyIsIm5iZiI6MTc0MzM1OTAxNC4wNCwic3ViIjoiNjdlOThjMjZiNTk5N2EwMjYxZTU0MWEyIiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.fU5nrIeHiVlKWpQ6XcP9KTWwuaPP-GbFFXa1re04Dpk",
-          },
-        };
-
-        fetch(url, options)
-          .then((res) => res.json())
-          .then((json) => console.log(json))
-          .catch((err) => console.error(err));
+        const res = await fetch(
+          `${MOVIE_BASE_URL}?api_key=${API_KEY}&language=en-US&page=1`
+        );
+        const data = await res.json();
+        const randomMovie =
+          data.results[Math.floor(Math.random() * data.results.length)];
+        setMovie(randomMovie);
       } catch (error) {
         console.error("Error fetching movies:", error);
       }
     })();
   }, []);
 
-  // const [movieList, setMovieList] = useState([]);
-  return <></>;
+  return (
+    <>
+      {movie ? (
+        <img
+          src={`${IMAGE_BASE_URL}${movie.backdrop_path}`}
+          alt={movie.title}
+          width={1920}
+          height={1080}
+          className="w-100"
+        />
+      ) : (
+        <p className="text-dark text-center fw-semibold mt-4">Loading...</p>
+      )}
+    </>
+  );
 }
 
 export default Hero;
